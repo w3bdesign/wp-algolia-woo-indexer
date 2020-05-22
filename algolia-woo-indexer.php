c5e7e9c74666ffc1afe50d3496ed3ff1a0eb4842
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-23 00:12:36 +0200

    Check if Woocommerce plugin is active

--- algolia-woo-indexer.php
+++ algolia-woo-indexer.php
@@ -9,23 +9,28 @@
  * @package         algolia-woo-indexer
  */
 
+
 require_once __DIR__ . '/vendor/autoload.php';
+if ( ! function_exists( 'is_plugin_active' ) ){
+    require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+}
 
 global $algolia;
 
 $algolia = \Algolia\AlgoliaSearch\SearchClient::create("undefined", "YourAdminApiKey");
 
-function is_woocommerce_plugin_active() {
-	return is_plugin_active( 'woocommerce/woocommerce.php' );
+function is_woocommerce_plugin_active()
+{
+    return in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')));
 }
 
 /**
  * If Woocommerce is not active, let users know.
  **/
-if ( ! is_woocommerce_plugin_active() ) {
-	add_action( 'admin_notices', function() {
-		echo '<div class="error notice">
-			  	<p>' . __( 'Algolia Woocommerce Indexer: <a href="' . admin_url( 'plugin-install.php?s=Woocommerce&tab=search&type=term' ) . '">Woocommerce</a> plugin should be enabled.', 'algolia-woo-indexer' ) . '</p>
+if (!is_woocommerce_plugin_active()) {
+    add_action('admin_notices', function () {
+        echo '<div class="error notice">
+			  	<p>' . __('Algolia Woocommerce Indexer: <a href="' . admin_url('plugin-install.php?s=Woocommerce&tab=search&type=term') . '">Woocommerce</a> plugin should be enabled.', 'algolia-woo-indexer') . '</p>
 		  	  </div>';
-	} );
+    });
 }
