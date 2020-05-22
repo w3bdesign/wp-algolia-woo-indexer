153c9f3eaef1a757128e9a02400d574f3105d924
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-22 03:09:38 +0200

    Check if Woocommerce is installed and enabled
    If not, display an error

--- algolia-woo-indexer.php
+++ algolia-woo-indexer.php
@@ -15,3 +15,17 @@ global $algolia;
 
 $algolia = \Algolia\AlgoliaSearch\SearchClient::create("undefined", "YourAdminApiKey");
 
+function is_woocommerce_plugin_active() {
+	return is_plugin_active( 'woocommerce/woocommerce.php' );
+}
+
+/**
+ * If Woocommerce is not active, let users know.
+ **/
+if ( ! is_woocommerce_plugin_active() ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error notice">
+			  	<p>' . __( 'Algolia Woocommerce Indexer: <a href="' . admin_url( 'plugin-install.php?s=Woocommerce&tab=search&type=term' ) . '">Woocommerce</a> plugin should be enabled.', 'algolia-woo-indexer' ) . '</p>
+		  	  </div>';
+	} );
+}
