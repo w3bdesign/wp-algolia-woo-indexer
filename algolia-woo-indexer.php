4f9dad05faa9bdc899efbb039540d61abc5b9050
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-19 23:19:49 +0200

    Testing PHPCS

--- algolia-woo-indexer.php
+++ algolia-woo-indexer.php
@@ -1,10 +1,3 @@
 <?php
 
-/**
- * Plugin Name:     Algolia Woocommerce Indexer
- * Description:     Implement Algolia indexing from Woocommerce
- * Text Domain:     algolia-woo-indexer
- * Version:         0.0.2
- *
- * @package         algolia-woo-indexer
- */
+echo "test";
