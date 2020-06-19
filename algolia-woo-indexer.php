8854498494c1b3da53e3a3152e3f112acfaf0b8c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-19 23:14:49 +0200

    Add plugin header

---
+++ algolia-woo-indexer.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name:     Algolia Woocommerce Indexer
+ * Description:     Implement Algolia indexing from Woocommerce
+ * Text Domain:     algolia-woo-indexer
+ * Version:         0.0.2
+ *
+ * @package         algolia-woo-indexer
+ */
