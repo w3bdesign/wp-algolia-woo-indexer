c95a8a2e2b7708ac1ce7f9a465fb61d07f13cd8c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-24 03:41:35 +0200

    Add uninstaller and add Algoliasearch PHP client

---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Delete the saved options freom the database when we uninstall the plugin
+ * We do not do it when we deactivate the plugin, in case we want to maintain the data
+ */
+
+define( 'ALGOWOO_DB_OPTION', '_algolia_woo_indexer' );
+
+delete_option( ALGOWOO_DB_OPTION . '_application_id' );
+delete_option( ALGOWOO_DB_OPTION . '_api_search_key' );
